2b56b773ec257718e7931986b9b1a8411dcdbad1
Jhonney Eduardo Correa <Jhonneycp@gmail.com>
2020-12-03 18:38:05 -0300

    formato de timeline añadido

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,29 @@
 import React from "react"
 import '../assets/styles/Menu.css'
+import meme from '../assets/static/meme.jpg'
+import hand from '../assets/static/hand.png'
+import repost from '../assets/static/repost.png'
+import box from '../assets/static/box.png'
+
 
 const Home = () =>{
     return(
-    <div className='Home'>Hola</div>
+    <main className="timeline">
+        <div className="post">
+            <picture>
+                <img src={meme} alt="Meme" />
+            </picture>
+            <section className="actions">
+                <div className="actions-left">
+                    <img src={hand} alt="" />
+                    <img src={repost} alt="" />
+                </div>
+                <div className="actions-right">
+                    <img src={box} alt="" />
+                </div>
+            </section>
+        </div>
+    </main>
     )
 }
 
